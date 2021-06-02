c30fc4d4105c20a952e531035f01161a0366ce62
DESKTOP-KV0FUTB\Tarek <tareknaous8@gmail.com>
2021-06-02 22:20:08 +0300

    updated functions

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,11 +1,113 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import cluster, datasets, mixture
-from scipy.spatial import ConvexHull, convex_hull_plot_2d
+from sklearn import datasets
 import alphashape
-from shapely.geometry import Polygon
-from shapely.ops import cascaded_union
 from scipy.spatial import ConvexHull, convex_hull_plot_2d
+from shapely.ops import cascaded_union
+
+
+def find_intersections(polygons):
+    'CONSIDER CLUSTERS SHOULD BE UNITED BASED ON PERCENTAGE OF INTERSECTION'
+    'Add in dictionary whether the intersection should be united or subtracted'
+    # Percentage threshold for uniting polygons
+    THRESHOLD = 30
+    # create empty dictionary
+    intersections = dict()
+
+    # create keys in dictionary
+    for i in range(0, len(polygons)):
+        key = i
+        intersections[key] = []
+
+    # Add intersections in the dictionary based on percentage criterion
+    for i in range(0, len(polygons)):
+        for j in range(i + 1, len(polygons)):
+            intersection_percentage = []
+            intersection_percentage.append((polygons[i].intersection(polygons[j]).area) / polygons[i].area * 100)
+            intersection_percentage.append((polygons[i].intersection(polygons[j]).area) / polygons[j].area * 100)
+
+            if polygons[i].intersects(polygons[j]) == True:
+                if intersection_percentage[0] >= THRESHOLD or intersection_percentage[0] >= THRESHOLD:
+                    key = i
+                    value = [j, 'union']
+                    intersections[key].append(value)
+                else:
+                    key = i
+                    value = [j, 'subtraction']
+                    intersections[key].append(value)
+
+    return intersections
+
+
+def return_unique_polygons(intersections):
+  'updated with union and subtraction criteria'
+  remove = [] #used to store index of keys to remove
+
+  #check which keys in the dictionary will need to be removed
+  for key in intersections:
+    for value in intersections[key]:
+      if value[0] in intersections:
+          remove.append(value[0])
+
+  #remove key from dictionary
+  for i in range(0,len(remove)):
+    #Add exception if code was trying to remove key that was already removed
+    try:
+      intersections.pop(remove[i])
+    except KeyError:
+      continue
+
+  return intersections
+
+
+from shapely.ops import cascaded_union
+
+
+def plot_new_polygons(unique_dictionary, polygons):
+    'Subtracts polygons with intersection % below threshold, and combine polygons with intersection % above threshold'
+
+    # Variable to decide whether to perform subtraction in case we have 3 or more intersecting polygons
+    need_subtract = False
+
+    for key in unique_dictionary:
+        # check if the key is empty (has not values)
+        if not unique_dictionary[key]:
+            # plot the polygon with no intersections
+            x, y = polygons[key].exterior.xy
+            plt.plot(x, y)
+
+        else:
+            # create an array to add the polygons to be merged
+            combination_merge = []
+            # added the polygon in the key itself
+            combination_merge.append(polygons[key])
+            # create an array to add the polygons to be subtracted, in case there is any
+            combination_substract = []
+
+            for value in unique_dictionary[key]:
+                if value[1] == 'union':
+                    combination_merge.append(polygons[value[0]])
+
+                elif value[1] == 'subtraction':
+                    combination_substract.append(polygons[value[0]])
+                    need_subtract = True
+
+            # merge the polygons to be merged
+            merged = cascaded_union(combination_merge)
+
+            # If no need to subtract, then just plot the merged polygons
+            if need_subtract == False:
+                x, y = merged.exterior.xy
+                plt.plot(x, y)
+
+            elif need_subtract == True:
+                # subtract the one to be subtracted from the merged ones
+                subtracted = []
+                for i in range(0, len(combination_substract)):
+                    subtracted.append(merged.symmetric_difference(combination_substract[i]))
+                    for j in range(0, len(subtracted[i])):
+                        x, y = subtracted[i][j].exterior.xy
+                        plt.plot(x, y)
 
 
 def plot_polygons(type, num_samples, num_clusters, random_state, *cluster_std, keep_points=False):
@@ -81,49 +183,4 @@ def plot_polygons(type, num_samples, num_clusters, random_state, *cluster_std, k
                 if keep_points == True:
                     plt.plot(points[i][0][:, 0], points[i][0][:, 1], 'o')  # Plot points
 
-
-def find_intersections(polygons):
-'CONSIDER CLUSTERS SHOULD BE UNITED BASED ONLY IF THEY INTERSECT'
-    # create empty dictionary
-    intersections = dict()
-    # create keys in dictionary
-    for i in range(0, len(polygons)):
-        key = i
-        intersections[key] = []
-
-    for i in range(0, len(polygons)):
-        for j in range(i + 1, len(polygons)):
-            if polygons[i].intersects(polygons[j]):
-                key = i
-                value = j
-                intersections[key].append(value)
-
-    return intersections
-
-def return_unique_polygons(intersections):
-  remove = []
-  for key in intersections:
-    for value in intersections[key]:
-      if value in intersections:
-        remove.append(value)
-
-  for i in range(0,len(remove)):
-    intersections.pop(remove[i])
-
-  return intersections
-
-
-def plot_new_polygons(unique_dictionary, polygons):
-  for key in unique_dictionary:
-    if not unique_dictionary[key]:
-      x,y = polygons[key].exterior.xy
-      plt.plot(x,y)
-    else:
-      combination = []
-      combination.append(polygons[key])
-      for value in unique_dictionary[key]:
-        combination.append(polygons[value])
-      merged = cascaded_union(combination)
-      x,y = merged.exterior.xy
-      plt.plot(x,y)
 
